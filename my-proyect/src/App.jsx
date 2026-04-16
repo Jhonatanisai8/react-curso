bbff72c4553de759410a8b9093528fdb5d24b3c9
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-04-15 23:39:17 -0500

    Atributos Reactivos

--- my-proyect/src/App.jsx
+++ my-proyect/src/App.jsx
@@ -14,12 +14,32 @@ function App() {
   const saySomething = () => {
     console.log("Something");
   };
+
+  const [miTexto, setMiTexto] = useState("Tu Nombre");
+  const [miValorTexto, setMiValorTexto] = useState("Valor");
+
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    setMiValorTexto(e.target.value);
+  };
   return (
     <>
       <Menu></Menu>
       <h1 onClick={saySomething}>Hola</h1>
       <h2>Numero: {numero}</h2>
       <button onClick={aumentar}>Aumentar Contador</button>
+      <hr />
+
+      <div>
+        <h1>Hola a todos</h1>
+        <h3>{miValorTexto}</h3>
+        <input
+          type="text"
+          placeholder={miTexto}
+          value={miValorTexto}
+          onChange={handleInput}
+        />
+      </div>
     </>
   );
 }
