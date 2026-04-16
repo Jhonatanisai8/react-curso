35743553de96c6e46b558e84e5966e68e83209a3
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-04-15 23:24:46 -0500

    eventos en react

--- my-proyect/src/App.jsx
+++ my-proyect/src/App.jsx
@@ -3,9 +3,13 @@ import Menu from "./components/Menu";
 
 console.log("Hola Mundo");
 function App() {
+  const saySomething = () => {
+    console.log("Something");
+  };
   return (
     <>
       <Menu></Menu>
+      <h1 onClick={saySomething}>Hola</h1>
     </>
   );
 }
