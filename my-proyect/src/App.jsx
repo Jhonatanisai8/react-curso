a4b6f1d0802b337d27da0f1949fc2256d15699ee
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-04-15 23:30:36 -0500

    useState

--- my-proyect/src/App.jsx
+++ my-proyect/src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import Menu from "./components/Menu";
 
 console.log("Hola Mundo");
 function App() {
+  const [numero, setNumero] = useState(0);
+
+  const aumentar = () => {
+    setNumero(numero + 1);
+    console.log(numero);
+  };
+
   const saySomething = () => {
     console.log("Something");
   };
@@ -10,6 +18,8 @@ function App() {
     <>
       <Menu></Menu>
       <h1 onClick={saySomething}>Hola</h1>
+      <h2>Numero: {numero}</h2>
+      <button onClick={aumentar}>Aumentar Contador</button>
     </>
   );
 }
