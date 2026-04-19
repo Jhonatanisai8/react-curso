d73175b0be5e43352a41fbb6ed68ee77e0fe33e2
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-04-19 12:58:09 -0500

    index en listas

--- my-proyect/src/components/RenderizadoListas.jsx
+++ my-proyect/src/components/RenderizadoListas.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 const frutas = ["Manzana", "Banana", "Naranja"];
-const HTMLfrutas = frutas.map((fruta) => {
-  return <li key={fruta}>{fruta}</li>;
+const HTMLfrutas = frutas.map((fruta, index) => {
+  return (
+    <li key={fruta}>
+      {index + 1}-{fruta}
+    </li>
+  );
 });
 const RenderizadoListas = () => {
   return (
     <div>
       <h2>Renderizado de listas</h2>
-      <ol>{HTMLfrutas} </ol>
+      <div>{HTMLfrutas} </div>
     </div>
   );
 };
