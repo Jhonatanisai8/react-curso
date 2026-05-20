c0b4c5eb4a06d9f42eb76d7fc95a0332e50b3c6b
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-20 00:27:00 -0500

    feat: argumento para que el useEffect se ejecute una sola vez

--- my-proyect/src/components/usseffct/Hijo.jsx
+++ my-proyect/src/components/usseffct/Hijo.jsx
@@ -6,14 +6,14 @@ const Hijo = () => {
   const [contador, setContador] = useState(0);
   useEffect(() => {
     console.log("Componente hijo se ah renderizado");
-  });
+  }, []);
   return (
     <div className=" mt-1 border border-b-amber-700 p-4 text-center  rounded">
       Este es un componente hijo
       <h3>{contador}</h3>
       <button
-        className="border
-      border-blue-500 bg-amber-700"
+        className="border p-5 rounded-2xl
+      border-blue-500 bg-amber-700 hover: cursor-pointer"
         onClick={() => {
           setContador(contador + 1);
         }}
