a445688d81bfe5fffca4376519ea1e4b3d16954c
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-20 00:24:51 -0500

    feat: ejemplo del contador para useEffect

--- my-proyect/src/components/usseffct/Hijo.jsx
+++ my-proyect/src/components/usseffct/Hijo.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 
 const Hijo = () => {
+  const [contador, setContador] = useState(0);
   useEffect(() => {
     console.log("Componente hijo se ah renderizado");
   });
   return (
     <div className=" mt-1 border border-b-amber-700 p-4 text-center  rounded">
       Este es un componente hijo
+      <h3>{contador}</h3>
+      <button
+        className="border
+      border-blue-500 bg-amber-700"
+        onClick={() => {
+          setContador(contador + 1);
+        }}
+      >
+        Aumentar Contador
+      </button>
     </div>
   );
 };
