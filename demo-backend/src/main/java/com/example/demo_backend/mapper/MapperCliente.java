3dc99e2c7400c5c19f870eab5a0c194f9f2bac6b
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-20 01:11:34 -0500

    feat: anotacion component para la clase que se encarga de convertir entidades a dtos

--- demo-backend/src/main/java/com/example/demo_backend/mapper/MapperCliente.java
+++ demo-backend/src/main/java/com/example/demo_backend/mapper/MapperCliente.java
@@ -1,8 +1,11 @@
 package com.example.demo_backend.mapper;
 
+import org.springframework.stereotype.Component;
+
 import com.example.demo_backend.dto.ClienteResponseDTO;
 import com.example.demo_backend.entity.Cliente;
 
+@Component
 public class MapperCliente {
     public ClienteResponseDTO toResponseDTO(Cliente cliente) {
         return new ClienteResponseDTO(
