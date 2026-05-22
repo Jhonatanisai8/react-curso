6c28688380dfc70eea6fc19d66df802053687382
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-21 23:13:21 -0500

    feat: refactor Example component layout for improved structure

--- my-proyect/src/components/fetch-api/Example.jsx
+++ my-proyect/src/components/fetch-api/Example.jsx
@@ -15,24 +15,26 @@ const Example = () => {
   return (
     <div className="flex items-center text-lg font-bold flex-col text-center">
       <h1>fetch | llamado a una api ....</h1>
-      <table className="w-full text-sm text-left rtl:text-right text-body">
-        <thead>
-          <tr>
-            <th className="px-6 py-3 font-medium">Nombres</th>
-            <th className="px-6 py-3 font-medium">Apellidos</th>
-            <th className="px-6 py-3 font-medium">Ciudad</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.nombres}</td>
-              <td>{user.apellidos}</td>
-              <td>{user.ciudad}</td>
+      <div className="m-1">
+        <table className="w-full text-sm text-left rtl:text-right text-body">
+          <thead>
+            <tr>
+              <th className="px-6 py-3 font-medium">Nombres</th>
+              <th className="px-6 py-3 font-medium">Apellidos</th>
+              <th className="px-6 py-3 font-medium">Ciudad</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <th className="px-6 py-3 font-medium">{user.nombres}</th>
+                <th className="px-6 py-3 font-medium">{user.apellidos}</th>
+                <th className="px-6 py-3 font-medium">{user.ciudad}</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
